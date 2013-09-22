a3f04e3a941ccc29d3895eb97738e6b79ff00f36
Wesley Reisz <wes@wesleyreisz.com>
2013-09-22 17:51:59 -0400

    added simple ui and button

--- class4/NoteTaker/src/com/wesleyreisz/teaching/android/class4/notetaker/MainActivity.java
+++ class4/NoteTaker/src/com/wesleyreisz/teaching/android/class4/notetaker/MainActivity.java
@@ -3,6 +3,9 @@ package com.wesleyreisz.teaching.android.class4.notetaker;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button btnSave = (Button)findViewById(R.id.btnNote);
+		btnSave.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				btnSave.setText("Clicked");
+			}
+		});
 	}
 
 	@Override
