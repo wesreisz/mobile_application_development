855bb239d806d50b1cf6c5c2e3acddf1bde3b74e
Wesley Reisz <wes@wesleyreisz.com>
2013-09-22 21:10:19 -0400

    added timestamp

--- class4/NoteTaker/src/com/wesleyreisz/teaching/android/class4/notetaker/MainActivity.java
+++ class4/NoteTaker/src/com/wesleyreisz/teaching/android/class4/notetaker/MainActivity.java
@@ -1,5 +1,10 @@
 package com.wesleyreisz.teaching.android.class4.notetaker;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -7,6 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private boolean isInEditMode = true;
@@ -29,8 +35,15 @@ public class MainActivity extends Activity {
 					txtTitle.setEnabled(false);
 					txtNotes.setEnabled(false);
 					btnSave.setText("Edit");
+					
+					TextView lastUpdated = (TextView)findViewById(R.id.txtLastUpdated);
+					DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+					String date = dateFormat.format(Calendar.getInstance().getTime());
+					lastUpdated.setText(date);
+					
 				}else{
 					isInEditMode=true;
+					
 					txtTitle.setEnabled(true);
 					txtNotes.setEnabled(true);
 					btnSave.setText("Save");
